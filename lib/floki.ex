28ede30196ced0e3df6a61fad7cd644ed56356f5
beppo <beppo@localhost.localdomain>
2020-12-27 20:37:26 -0600

    album_links, format_seller_info

--- lib/floki.ex
+++ lib/floki.ex
@@ -6,9 +6,27 @@ defmodule Floki do
     case fetch_html(@index_page) do
       {:ok, html} ->
         #get links, parse info
+        html
+        |> album_links()
+        |> Stream.map(&fetch_html/1)
       {:error, msg} ->
         msg
     end
+  end
+
+  defp format_seller_info(html) do
+    case html do
+      {:ok, body} ->
+        # some code
+      {:error, msg} ->
+        msg
+    end
+  end
+
+  defp album_links(html) do
+    html
+    |> Floki.find(".table tr a")
+    |> Floki.attribute("href")
   end
 
   defp fetch_html(url) do
